62e97a26cad6b71e941274dbd5a9bba748a07b20
MiandryR <75442649+MiandryR@users.noreply.github.com>
2021-04-04 15:57:56 +0200

    Styling css

--- src/Results.js
+++ src/Results.js
@@ -2,10 +2,13 @@ import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
 
+import './Results.css';
+
 export default function Results(props) {
     if (props.results) {
         return (
             <div className="results">
+                <section>
                 <h1 className="word">
                     {props.results.word}
                 </h1>
@@ -17,13 +20,16 @@ export default function Results(props) {
                         </div>
                     )
                 })}
+                </section>
                 <br />
-                    {props.results.meanings.map(function (meaning, index) {
+                                    {props.results.meanings.map(function (meaning, index) {
                             return (
                                 <div key={index}>
                                     <Meaning meaning={meaning} />
                                 </div>
-                            )
+                                
+                        )
+                        
                         })
                     }
             </div>
