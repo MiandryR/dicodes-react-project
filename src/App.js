05d5df0783f09981274c4799ac17a12fef5a247e
MiandryR <75442649+MiandryR@users.noreply.github.com>
2021-04-05 15:11:55 +0200

    Added Photos

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
           </p>
         </header>
         <br/> 
-        <Dictionary />
+        <Dictionary defaultKeyword="success"/>
       </div>
     <Footer />
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,59 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
     
-    function handleResponse(response) {
-        console.log(response.data[0]);
+    function handleDicoResponse(response) {
         setResults(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
-   
+
+    function search() {
+        let dicoApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(dicoApiUrl).then(handleDicoResponse);
+
+        let pexelsApiKey = `563492ad6f91700001000001199ada6b14654ae981c457220f6e5fe5`;
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
 
     function handleKeyword(event) {
-        console.log(event.target.value);
         setKeyword(event.target.value);
     }
-
-    return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-            <input type="search" placeholder="Enter a word" onChange={handleKeyword}/>
-            </form>
-            <Results results={results}/>
-        </div>
-    );
+    function load() {
+        setLoaded(true);
+        search();
+    }
+    
+    if (loaded) {
+        return (
+            <div className="Dictionary">
+                <h5>The meaning of:</h5>
+                <form onSubmit={handleSubmit}>
+                    <input type="search" placeholder="Enter a word" onChange={handleKeyword} />
+                </form>
+                <div className="hint">i.e beta, life, exchange, submarine, warfare </div>
+                <Results results={results} />
+                <Photos photos={photos} />
+            </div>
+        );
+    } else {
+        load();
+        return "Loading";
+    }
 }
